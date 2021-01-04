93c25cb253df1d7ac3041d7ef7e060c596364d49
Rayan Frigui <46843728+rayanfrigui@users.noreply.github.com>
2021-01-04 17:38:58 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+def on_button_pressed_ab():
+    basic.show_icon(IconNames.TSHIRT)
+    basic.clear_screen()
---
+++ main.ts
@@ -0,0 +1,100 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(8)
+})
+input.onButtonPressed(Button.AB, function () {
+    soundExpression.giggle.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Mazambe")
+})
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . . #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    # # # # .
+    # . # . .
+    # # # . .
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # . . . #
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showLeds(`
+    # . # . #
+    # . # . #
+    # . # . #
+    # . # . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # . . .
+    . # . . .
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
